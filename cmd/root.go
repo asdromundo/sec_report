31f15001b3aafa92c9fb0772b620a40734010dd7
Antonio S. Dromundo <sebastiandromundo@outlook.com>
2024-02-27 23:45:32 -0600

    Whois funcionando

--- cmd/root.go
+++ cmd/root.go
@@ -17,28 +17,98 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
+	"github.com/asdromundo/sec_report/pkg/pentesting"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+)
+
+var (
+	pingFlag       bool
+	nslookupFlag   bool
+	tracerouteFlag bool
+	whoisFlag      bool
+	sublist3rFlag  bool
+	subfinderFlag  bool
+	findomainFlag  bool
+	dnsmapFlag     bool
+	dnsreconFlag   bool
+	nmapFlag       bool
+	etherapeFlag   bool
+	outputFlag     string
 )
 
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "sec_report",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "sec_report [domain or IP]",
+	Short: "Generate a security report for a given domain or IP",
+	Long: `sec_report es una herramienta de línea de comandos diseñada para generar un informe de seguridad exhaustivo para un dominio o una dirección IP específica. Esta aplicación utiliza una variedad de herramientas de red y DNS para recopilar información valiosa y proporcionar una visión completa de la postura de seguridad de un sistema.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Las siguientes aplicaciones se utilizan para recopilar datos relevantes durante el proceso de generación de informes:
+
+ping: Realiza pruebas de conectividad básicas y mide el tiempo de respuesta entre el sistema local y el destino especificado.
+nslookup: Proporciona información de resolución de nombres, como direcciones IP y registros de DNS asociados con un nombre de dominio.
+traceroute: Muestra la ruta que los paquetes de red toman desde el sistema local hasta el destino especificado, identificando los saltos intermedios.
+whois: Permite obtener información sobre el propietario y otros detalles de un nombre de dominio o una dirección IP.
+sublist3r: Utiliza la técnica de enumeración de subdominios para descubrir subdominios asociados con el dominio especificado.
+subfinder: Similar a sublist3r, subfinder también se utiliza para enumerar subdominios y descubrir posibles puntos de entrada adicionales.
+findomain: Otra herramienta de enumeración de subdominios que ayuda a descubrir subdominios adicionales asociados con el dominio especificado.
+dnsmap: Realiza un análisis de mapeo de DNS, identificando las direcciones IP asociadas con nombres de dominio específicos.
+dnsrecon: Proporciona funcionalidades avanzadas para la enumeración de subdominios y la recopilación de información de DNS.
+nmap: Una herramienta de escaneo de puertos y detección de servicios que identifica los servicios disponibles en los sistemas de destino.
+etherApe: Proporciona una representación visual de la actividad de red, ayudando a identificar patrones y anomalías.
+
+sec_report integra estas herramientas de forma eficiente para recopilar y analizar datos de manera automatizada, proporcionando a los usuarios un informe detallado sobre la seguridad y la infraestructura de red asociada con el dominio o la dirección IP proporcionados.
+	`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// Check if there are any arguments passed
+		if len(args) < 1 || !pentesting.IsValidURL(args[0]) {
+			fmt.Println("Error: Please provide a valid domain or IP address.")
+			fmt.Println(cmd.UsageString())
+			os.Exit(1)
+		}
+
+		domainOrIP := args[0]
+		fmt.Printf("Generating security report for: %s\n", domainOrIP)
+
+		var results bytes.Buffer
+
+		// Perform tasks based on flags
+		if pingFlag {
+			fmt.Println("Performing ping...")
+		}
+		if nslookupFlag {
+			fmt.Println("Performing nslookup...")
+		}
+		if whoisFlag {
+			result, err := pentesting.Whois(domainOrIP)
+			// Imprimir el resultado
+			if err != nil {
+				fmt.Printf("Error al ejecutar whois: %v\n", err)
+				return
+			}
+			results.WriteString(fmt.Sprintf("Información de whois para la IP %s\n\n", domainOrIP))
+			results.WriteString(result)
+		}
+
+		if outputFlag == "" {
+			fmt.Println(results.String())
+		} else {
+			// Save results to file
+			if err := pentesting.SaveResultsToFile(results, outputFlag); err != nil {
+				fmt.Printf("Error al guardar los resultados en el archivo: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Printf("Resultados guardados en: %s\n", outputFlag)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -62,6 +132,20 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	rootCmd.PersistentFlags().BoolVar(&pingFlag, "ping", false, "Perform a ping test")
+	rootCmd.PersistentFlags().BoolVar(&nslookupFlag, "nslookup", false, "Perform a DNS lookup")
+	rootCmd.PersistentFlags().BoolVar(&tracerouteFlag, "traceroute", false, "Perform a traceroute")
+	rootCmd.PersistentFlags().BoolVar(&whoisFlag, "whois", true, "Perform a WHOIS lookup")
+	rootCmd.PersistentFlags().BoolVar(&sublist3rFlag, "sublist3r", false, "Perform a sublist3r lookup")
+	rootCmd.PersistentFlags().BoolVar(&subfinderFlag, "subfinder", false, "Perform a subfinder lookup")
+	rootCmd.PersistentFlags().BoolVar(&findomainFlag, "findomain", false, "Perform a findomain lookup")
+	rootCmd.PersistentFlags().BoolVar(&dnsmapFlag, "dnsmap", false, "Perform a dnsmap lookup")
+	rootCmd.PersistentFlags().BoolVar(&dnsreconFlag, "dnsrecon", false, "Perform a dnsrecon lookup")
+	rootCmd.PersistentFlags().BoolVar(&nmapFlag, "nmap", false, "Perform an Nmap scan")
+	rootCmd.PersistentFlags().BoolVar(&etherapeFlag, "etherape", false, "Open EtherApe for network visualization")
+	rootCmd.PersistentFlags().StringVarP(&outputFlag, "output", "o", "", "Output HTML report file")
+
 }
 
 // initConfig reads in config file and ENV variables if set.
--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module github.com/asdromundo/sec_report
 go 1.22.0
 
 require (
+	github.com/davidmytton/url-verifier v1.0.1
+	github.com/spf13/afero v1.11.0
+	github.com/spf13/cobra v1.8.0
+	github.com/spf13/viper v1.18.2
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -12,11 +20,8 @@ require (
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.18.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
